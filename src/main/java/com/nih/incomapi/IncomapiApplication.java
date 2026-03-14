a3b2a3621c4cb8df618788b3b465d117d65986b6
Baiju Achari <baijuachari2025@gmail.com>
2026-03-14 09:12:34 -0400

    initial version

---
+++ src/main/java/com/nih/incomapi/IncomapiApplication.java
@@ -0,0 +1,13 @@
+package com.nih.incomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nih/incomapi/IncomapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nih.incomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
